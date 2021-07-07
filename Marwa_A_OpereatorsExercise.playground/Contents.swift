943d619fcdc33711d4aa89a52269bc9aa58df256
MarwaAbdul <86677401+MarwaAbdul@users.noreply.github.com>
2021-07-07 06:02:40 -0500

    Add files via upload

---
+++ Marwa_A_OpereatorsExercise.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+//Section 1 (establishing numbers/variables)
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+var e = 5
+var f = 6
+var g = 7
+var h = 8
+var i = 9
+var j = 0
+
+//Section 2 (defining operations)
+//Addition
+print (a + b)
+print (f + c)
+print (h + j)
+print (d + g)
+
+//Subtraction
+print (h - a)
+print (e - j)
+print (i - d)
+print (f - c)
+
+//Multiplication
+print (f * d)
+print (a * h)
+print (c * i)
+print (j * g)
+
+//Division
+print (i / c)
+print (h / d)
+print ( j / a)
+print ( f / b)
+
+//Section 3? (output) [not sure how to format this if answers are already on right of screen]
+//Is it referring to the answer bar located at the bottom of the screen as well?
