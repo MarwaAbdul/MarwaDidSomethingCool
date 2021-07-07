443c36842e46e59d4c9b5fa06f948bd3626d9a9a
MarwaAbdul <86677401+MarwaAbdul@users.noreply.github.com>
2021-07-07 06:04:21 -0500

    Add files via upload

---
+++ Variables.playground/Pages/Exercise-Shopping List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*:
+ ## Exercise: Making a Shopping List
+
+ The constants below represent some of the things you might want to add to a shopping list:
+*/
+let eggs = "Eggs"
+let milk = "Milk"
+let cheese = "Cheese"
+let bread = "Bread"
+let rice = "Rice"
+let newLine = "\n"
+let check = "✔️"
+//: - callout(Exercise): Create a string variable with an initial value of `""`. Add each constant item above to the list, one at a time. Add a `newLine` in between each item. Remember you can join two strings using the `+` operator.
+let shoppingListString = "-Eggs \(check)\(newLine)-Milk \(check)\(newLine)-Cheese \(check)\(newLine)-Bread \(check)\(newLine)-Rice\(check) "
+
+
+
+
+
+//: [Previous](@previous)  |  page 12 of 13  |  [Next: Exercise: 501](@next)
