e4f18190f5b83461bace5c532adff5eb5b71a587
MarwaAbdul <86677401+MarwaAbdul@users.noreply.github.com>
2021-07-12 01:52:39 -0500

    Add files via upload

---
+++ Marwa_A_ControlFlowExercise.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//Control Flow Egg timer
+
+//Variables and constants (180 = 3 min)
+var totalTime = 180
+
+let stop = "Ding Dong, the king is dead"
+
+//Control Flow
+
+for timer in 0...totalTime{
+
+    
+print ("\timer) in seconds")
+
+    if timer == totalTime{
+    print (stop)
+
+
+    
+}
+}
