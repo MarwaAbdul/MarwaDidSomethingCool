86c41aa94413aacd69921ac806b0fd0999c88ad4
MarwaAbdul <86677401+MarwaAbdul@users.noreply.github.com>
2021-07-12 07:22:42 -0500

    Add files via upload

---
+++ Marwa_A_CollectionExercise.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+//Collection list (road maps)
+
+var collectionList: [String] = ["Illinois"]
+
+
+
+
+//Practice With add/equal append array
+
+collectionList += ["Michigan"]
+collectionList += ["Indiana"]
+collectionList += ["Ohio"]
+collectionList += ["Kentucky"]
+
+
+//Practice with regular append array
+collectionList.append("Virginia")
+collectionList.append("West Virginia")
+collectionList.append("North Carolina")
+collectionList.append("California")
+collectionList.append("Oregon")
+
+//I don't actually own this many road maps yet because I don't go places, but people I know are travelling and said they'd bring me them.
+
+print("I like to collect road maps. So far, I have \(collectionList.count) ")
+
+
