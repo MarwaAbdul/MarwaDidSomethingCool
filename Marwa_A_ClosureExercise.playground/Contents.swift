01cb5f9a60481118237be49134f5796488b3333c
MarwaAbdul <86677401+MarwaAbdul@users.noreply.github.com>
2021-07-14 11:36:46 -0500

    Add files via upload

---
+++ Marwa_A_ClosureExercise.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//Values
+var val1 = 48
+var val2 = 25
+let answer = 48 * 25
+
+//Closure expression
+let multi = {
+    (val1: Int, val2: Int) -> Int in
+    return val1 * val2
+   
+    
+
+}
+print (answer)
